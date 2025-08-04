4bc55a41c3f2f2d9be20067ac85b8c716e95dcca
Mehedi Hasan Shojib <hello@shojib.eu.org>
2025-08-04 18:08:33 +0600

    chore: add Source URL to project metadata in pyproject.toml
    ## Summary by Sourcery
    
    Enhancements:
    - Add a Source URL entry to the project URLs in pyproject.toml

--- src/truelink/__init__.py
+++ src/truelink/__init__.py
@@ -6,7 +6,7 @@ from .core import TrueLinkResolver
 from .exceptions import TrueLinkException, UnsupportedProviderException
 from .types import FolderResult, LinkResult
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 __all__ = [
     "FolderResult",
     "LinkResult",
